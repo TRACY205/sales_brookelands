2ebb295d20b490e34b64ef68b82495114bab6eb6
TRACY205 <tracyirungu8@gmail.com>
2025-09-03 09:40:11 +0300

    Updated sales report template with totals

--- myapp/models.py
+++ myapp/models.py
@@ -5,9 +5,9 @@ class Sale(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.CharField(max_length=100)
     item = models.CharField(max_length=100)
-    quantity = models.PositiveIntegerField()
+    quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.item} - {self.user.username}"
+        return f"{self.item} - {self.quantity}"
